12012f2a5bf6112a4f54df5852607ee91d5c3dd6
EmreKARABLT <emrekarablt53@gmail.com>
2023-02-15 15:09:24 +0100

    Parsing Template Rule is completed

--- src/main/java/Rule.java
+++ src/main/java/Rule.java
@@ -1,48 +1,91 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Rule {
 	public String template;
-	public HashMap<String,ArrayList<String>> slotsAndActions;
-	public Rule(String template, HashMap<String, ArrayList<String>> slotsAndActions){
+	public HashMap< String, ArrayList<String> > slots;
+	public ArrayList<String> actions;
+	public ArrayList< ArrayList<String> > pairedValues = new ArrayList<>();
+	public ArrayList<String> parsedRules = new ArrayList<>();
+
+
+	public Rule(String template, HashMap<String, ArrayList<String>> slots , ArrayList<String> actions){
 		this.template = template.toLowerCase(Locale.ROOT);
-		this.slotsAndActions = slotsAndActions;
+		this.slots = slots;
+		this.actions = actions;
+		pairValues(this.parsedRules,0);
+		parseRule();
+	}
+
+	public String replaceKeyWithValue(String rule , String key, String value){
+		Pattern pattern = Pattern.compile(String.format("<%s>", key), Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(rule);
+		return matcher.replaceAll(value).toLowerCase(Locale.ROOT);
 	}
 	public ArrayList<String> parseRule(){
-		ArrayList<String> parsedStrings = new ArrayList<>();
-		for (String key : slotsAndActions.keySet()) {
-			if(!key.equalsIgnoreCase("action")){
-				for (String value :
-						slotsAndActions.get(key)) {
-					Pattern pattern = Pattern.compile(String.format("<%s>",key), Pattern.CASE_INSENSITIVE);
-					Matcher matcher = pattern.matcher(template);
-					System.out.println(value);
-					String parsedString = matcher.replaceFirst(value).toLowerCase(Locale.ROOT);
-					parsedStrings.add(parsedString);
-				}
+		parsedRules = new ArrayList<>();
+		String[] keys = slots.keySet().toArray(new String[0]);
+		for (int i = 0; i < pairedValues.size(); i++) {
+			String text = template;
+			for (int j = 0 ; j < keys.length ; j++ ) {
+				text = replaceKeyWithValue(text , keys[j] , pairedValues.get(i).get(j));
+			}
+			parsedRules.add(text);
+		}
+		return parsedRules;
+	}
+	private void pairValues(ArrayList<String> currentPair, int currentKeyIndex) {
+		if (currentPair.size() == slots.size()) {
+			pairedValues.add(currentPair);
+			return;
+		}
+		ArrayList<String> keys = new ArrayList<>(slots.keySet());
+		for (int i = currentKeyIndex; i < keys.size(); i++) {
+			String key = keys.get(i);
+			for (String value : slots.get(key)) {
+				ArrayList<String> newPair = new ArrayList<>(currentPair);
+				newPair.add(value);
+				pairValues(newPair, i + 1);
 			}
 		}
-		return parsedStrings;
 	}
 	public boolean isMatched(String question){
-		// Check that if a given input(question) is matched with parsed rule ( ruleParser )
+		for (int i = 0; i < parsedRules.size(); i++) {
+			if(parsedRules.get(i).contains(question.toLowerCase(Locale.ROOT))){
+				return true;
+			}
+		}
 		return false;
 	}
+
+	public boolean isMatchedUsingRegex(String question){
+//		System.out.println(TextProcessor.regExForSearching(template));
+		Pattern pattern = Pattern.compile(TextProcessor.regExForSearching(question),Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(question);
+//		System.out.println(matcher);
+		return true;
+	}
+
 	public String findAction(String question){
-		// Among the all actions find the one which is suitable and return it
 		return " ";
+	}
+	public void addSlot(String key, ArrayList<String> values){
+		slots.put(key , values);
+		parseRule();
+	}
+	public void removeSlot(String key, ArrayList<String> value){
+		slots.remove(key,value);
+		parseRule();
 	}
 
 	public static void main(String[] args) {
-		HashMap<String , ArrayList<String>> slotsAndActions = new HashMap<>();
-		slotsAndActions.put("day",new ArrayList<String>(Arrays.asList("Monday","Tuesday","Friday","Saturday")));
+		HashMap<String , ArrayList<String>> slots = new HashMap<>();
+		slots.put("day",new ArrayList<String>(Arrays.asList("Monday","Tuesday")));
+		slots.put("time",new ArrayList<String>(Arrays.asList("1","2")));
 
-		Rule rule = new Rule("today is <DAY>", slotsAndActions );
-		System.out.println(rule.parseRule());
+		Rule rule = new Rule("Which lectures are there on <DAY> at <TIME>", slots , null );
+		System.out.println(rule.isMatchedUsingRegex("Which lectures are there on Monday"));
 	}
 }
 
--- src/main/java/TextProcessor.java
+++ src/main/java/TextProcessor.java
@@ -1,15 +1,24 @@
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextProcessor {
+	public static String[] propositions = new String[]{"aboard" ,"about" ,"above" ,"across" ,"after" ,"against" ,"along" ,"amid" ,"among" ,"anti" ,"around" ,"as" ,"at" ,"before" ,"behind" ,"below" ,"beneath" ,"beside" ,"besides" ,"between" ,"beyond" ,"but" ,"by" ,"concerning" ,"considering" ,"despite" ,"down" ,"during" ,"except" ,"excepting" ,"excluding" ,"following" ,"for" ,"from" ,"in" ,"inside" ,"into" ,"like" ,"minus" ,"near" ,"of" ,"off" ,"on" ,"onto" ,"opposite" ,"outside" ,"over" ,"past" ,"per" ,"plus" ,"regarding" ,"round" ,"save" ,"since" ,"than" ,"through" ,"to" ,"toward" ,"towards" ,"under" ,"underneath" ,"unlike" ,"until" ,"up" ,"upon" ,"versus" ,"via" ,"with" ,"within" ,"without"};
+	private static String[] questionPronouns = new String[]{"when", "where", "who", "whom", "which", "why", "what"};
+
+	public static String regExForSearching(String template) {
+
+		ArrayList<String> partitionedTemplate = splitSentenceRegexSearch(template);
+		String s = splitArrayToString(partitionedTemplate);
+		Pattern pattern = Pattern.compile("\\s*<.+?>\\s*");
+		Matcher matcher = pattern.matcher(s);
+
+		return "("+matcher.replaceAll("|")+")";
+	}
+
 	public static Boolean isQuestion(String question) {
 		question = question.toLowerCase(Locale.ROOT);
-		String[] questionPronouns = new String[]{"when", "where", "who", "whom", "which", "why", "what"};
 
 		for (String questionPronoun : questionPronouns) {
 			if (question.contains(questionPronoun)) {
@@ -18,6 +27,16 @@ public class TextProcessor {
 		}
 		return false;
 	}
+	public static ArrayList<String> splitSentenceRegexSearch(String sentence){
+//		Pattern pattern = Pattern.compile("([^A-z0-9\\s])", Pattern.CASE_INSENSITIVE);
+			Pattern pattern = Pattern.compile("[^\\w\\s<>\\d]", Pattern.CASE_INSENSITIVE);
+			Matcher matcher = pattern.matcher(sentence);
+			sentence = matcher.replaceAll(" ").toLowerCase(Locale.ROOT);
+			ArrayList<String> split = new ArrayList<>();
+			String[] q = sentence.split("\s+");
+			Collections.addAll(split, q);
+			return split;
+	}
 	public static ArrayList<String> splitSentence(String question){
 		Pattern pattern = Pattern.compile("[^A-z0-9\\s]", Pattern.CASE_INSENSITIVE);
 		Matcher matcher = pattern.matcher(question);
@@ -25,17 +44,11 @@ public class TextProcessor {
 		System.out.println(question);
 		ArrayList<String> split = new ArrayList<>();
 		String[] q = question.split("[*\s]");
+		ArrayList<String> pro= new ArrayList<String>(List.of(propositions));
+		ArrayList<String> qp= new ArrayList<String>(List.of(questionPronouns));
 		for (String word : q) {
 			if (!word.isEmpty()
-//					&& !word.equalsIgnoreCase("for")
-//					&& !word.equalsIgnoreCase("and")
-//					&& !word.equalsIgnoreCase("that")
-//					&& !word.equalsIgnoreCase("of")
-//					&& !word.equalsIgnoreCase("in")
-//					&& !word.equalsIgnoreCase("the")
-//					&& !word.equalsIgnoreCase("a")
-//					&& !word.equalsIgnoreCase("is")
-//					&& !word.equalsIgnoreCase("are")
+//					&& !pro.contains(word)&&!qp.contains(word)
 			) {
 				split.add(word);
 			}
@@ -44,8 +57,23 @@ public class TextProcessor {
 		return split;
 	}
 
+	public static String splitArrayToString(ArrayList<String> splitSentence){
+		StringBuilder combinedString = new StringBuilder();
+		for (int i = 0 ; i < splitSentence.size() ; i++){
+			combinedString.append(splitSentence.get(i));
+			if(i < splitSentence.size()-1)
+				combinedString.append(" ");
+		}
+		return combinedString.toString();
+	}
+
 	public static void main(String[] args) {
-		String q = "Sure! In regular expression, the special characters for punctuation and,space 10! are and p{P} respectively. Here is a regular expression that will match all punctuation and space characters:";
+		String q = "Which lectures are there on Monday at 9?";
+//		String temp = "Which lectures are there on <DAY> at <TIME>?";
+//		System.out.println(regExForSearching(temp));
 		System.out.println(splitSentence(q));
+		System.out.println(isQuestion(q));
+		System.out.println();
+
 	}
 }
