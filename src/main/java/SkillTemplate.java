abb3ecac0827875174f6ddac94c3ea0bc53eaa02
EmreKARABLT <emrekarablt53@gmail.com>
2023-03-22 19:00:53 +0100

    distance added

--- src/main/java/SkillTemplate.java
+++ src/main/java/SkillTemplate.java
@@ -20,7 +20,8 @@ public class SkillTemplate {
         this.actions = actions;
         this.pairedValues = new ArrayList<>();
         this.parsedRules = new ArrayList<>();
-
+        pairValues(this.parsedRules,0);
+        parseRule();
     }
 
     public void addAllSlots(){
